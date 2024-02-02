58b0579515c6368753cfa9f8654af2d76c687e26
SimranjeetKaur21 <157611847+SimranjeetKaur21@users.noreply.github.com>
2024-02-02 01:32:07 -0500

    Update CardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -7,14 +7,7 @@ package card;
 import java.util.Scanner;
 
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects
- * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
- * @author srinivsi
- */
-
-/**
- * A CardTrick class models a "hand" of seven cards filled with random card objects
+ * A CardTrick class that fills a magic hand of seven cards with random card objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. 
  * Name: SimranjeetKaur
@@ -36,12 +29,13 @@ public class CardTrick {
             magicHand[i]=c;
         }
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
         // add one luckcard hard code 2,clubs
-       
-        
+        Card luckyCard = new Card();
+        luckyCard.setValue(10);
+        luckyCard.setSuit(Card.SUITS[2]);
+        magicHand[magicHand.length - 1]=luckyCard;
+
+        //insert code to ask the user for Card value and suit, create their card
         Scanner scanner = new Scanner(System.in);
         System.out.print("pick the card value: ");
         int userValue = scanner.nextInt();
@@ -52,6 +46,7 @@ public class CardTrick {
         userCard.setValue(userValue);
         userCard.setSuit(Card.SUITS[userSuit]);
         
+        // and search magicHand here
         boolean isCardInMagicHand = false;
         for (Card card : magicHand){
             if (card.equals(userCard)){
@@ -59,14 +54,14 @@ public class CardTrick {
                 break;
             }
         }
-        
+        //Then report the result here
         if (isCardInMagicHand){
             System.out.println("congratulations! your card is in the magic hand.");
         }else{
             System.out.println("Sorry, your card is not in magic hand.");
         }
     }
-    
+     
     private static int generateRandomNumber(int min, int max){
         return (int)(Math.random() * (max-min + 1)+ min);
     }
